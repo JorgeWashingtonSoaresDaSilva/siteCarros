ff415d22e23f0ffacd850e520c538a40cc5ddf0d
JorgeWashingtonSoaresDaSilva <jwss486@gmail.com>
2021-10-14 06:09:06 -0300

    Criação da classe Funcionario com seus atributos e geters e seters

---
+++ bancoGerente/src/entities/Funcionario.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Funcionario {
+	// Atributos
+	private String nome;
+	private double salarioBruto;
+	private String cargo;
+	
+	// Métodos Geters e Seters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalarioBruto() {
+		return salarioBruto;
+	}
+	public void setSalarioBruto(double salarioBruto) {
+		this.salarioBruto = salarioBruto;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	// Metodos de regra de negocio
+	
+}
